3e793a48b4e505994971afb1fd7c9528ad23e800
Hritik Kawale <153885259+hritik3345@users.noreply.github.com>
2025-07-02 16:14:34 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 //api
-const API_KEY = '1a03b66d70904a868b8d2b9717659bf2_70496d7b91584b8cbcad7788df109843_andoraitools';
+const API_KEY = 'a6c1d363e15547bd9e58134edd49e90d_90b0e44d1e024495855a22217b79b202_andoraitools';
 
 // Root route
 app.get('/', (req, res) => {
